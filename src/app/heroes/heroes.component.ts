d1bc64b26f80bee492b75670fa06090b387dbf65
Juan José Londoño <33065960+juanjolondo@users.noreply.github.com>
2019-12-06 21:47:18 -0500

    Sexto punto del tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,4 +20,15 @@ export class HeroesComponent implements OnInit {
     this.heroSevice.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void{
+    name = name.trim();
+    if (!name) {return;}
+    this.heroSevice.addHero({ name } as Hero)
+      .subscribe(hero => {this.heroes.push(hero)})
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroSevice.deleteHero(hero).subscribe();
+  }
 }
